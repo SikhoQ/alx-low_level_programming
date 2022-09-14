bf3e27eee2512dbcc3294cbec305873b466a268b
SikhoQ <qangulesikho@gmail.com>
2022-09-14 16:38:02 -0700

    fixed two-digit printing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,13 @@ void	times_table(void)
 		num2 = 0;
 		while (num2 < 10)
 		{
-			_putchar((num1 * num2) + '0');
+			if (num1 * num2 < 10)
+				_putchar((num1 * num2) + '0');
+			else
+			{
+				_putchar(((num1 * num2) / 10) + '0');
+				_putchar(((num1 * num2) % 10) + '0');
+			}
 			if (num2 < 9)
 			{
 				_putchar(',');
