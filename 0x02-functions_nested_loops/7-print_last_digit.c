9999880865ecd7c0f564cb2df23915fcf8497f7a
SikhoQ <qangulesikho@gmail.com>
2022-09-14 15:36:48 -0700

    corrected function to support negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,19 @@
 /**
  * print_last_digit - print the last digit of the number given as argument
  * @n: number whose last digit is to be printed
- * @Return: the last digit of the number given as argument
+ * Return: the last digit of the number given as argument
  */
 int	print_last_digit(int n)
 {
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int	digit;
+
+	digit = n % 10;
+
+	if (digit < 0)
+		digit *= -1;
+
+	_putchar(digit + '0');
+
+	return (digit);
 }
 
