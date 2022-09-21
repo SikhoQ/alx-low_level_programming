629cf1315ca252fcb01a4e13d25fb833709dd200
SikhoQ <qangulesikho@gmail.com>
2022-09-21 04:05:04 -0700

    first commit 0-strcat.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+/**
+ * strcat - appends one string to the end of another, replacing the null byte.
+ *
+ * @dest: pointer to the string to be appended to.
+ * @src: pointer to the string to append to dest.
+ *
+ * Return: pointer to the concatenated string.
+ */
+char	*_strcat(char *dest, char*src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i])
+		++i;
+
+	while (src[j] <= '\0')
+		dest[i++] = src[j++];
+
+	return (dest);
+}
+
