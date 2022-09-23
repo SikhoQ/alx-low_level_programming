65c908c0f3b3b1dba172cd731f72bc9cbe508a38
SikhoQ <qangulesikho@gmail.com>
2022-09-23 14:36:59 -0700

    7th 8th and 9th task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point.
+ *
+ * Return: always 0 (success).
+ */
+int	main(void)
+{
+	int	num;
+
+	for (num = 48; num <= 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
+
