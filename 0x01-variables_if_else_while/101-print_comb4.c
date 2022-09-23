2ec69507b26b2b48fd1dacf6080ab7b94bd7a328
SikhoQ <qangulesikho@gmail.com>
2022-09-23 15:01:01 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,8 +9,8 @@ int	main(void)
 {
 	int	num1, num2, num3;
 
-	for (num1 = 48; num < 58; num1++)
-		for (num2 = num1 + 1; num < 58; num2++)
+	for (num1 = 48; num1 < 58; num1++)
+		for (num2 = num1 + 1; num2 < 58; num2++)
 			for (num3 = num2 + 1; num3 < 58; num3++)
 			{
 				if (num1 != 55)
