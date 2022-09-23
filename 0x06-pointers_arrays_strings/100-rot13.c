79aac2f57814f99e4ae719cecfe8b3332c831915
SikhoQ <qangulesikho@gmail.com>
2022-09-23 15:59:39 -0700

    rotation

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @str: pointer to the string to be encoded.
+ *
+ * Return: encoded string, str.
+ */
+char	*rot13(char *str)
+{
+	int	i;
+	int	j;
+	char	*alpha = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	for (i = 0; str[i]; i++)
+		for (j = 0; alpha[j]; j++)
+			if (str[i] == alpha[j])
+				str[i] = alpha[(j + 26) % 52];
+	return (str);
+}
+
