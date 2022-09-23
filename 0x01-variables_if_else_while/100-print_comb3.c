6ea3e4a3c3732b42f77cb99b2aeb05aad6f8ec1f
SikhoQ <qangulesikho@gmail.com>
2022-09-23 14:55:50 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int	main(void)
 	int	num1;
 	int	num2;
 
-	for (num1 = 48; num1 < 58, num1++)
+	for (num1 = 48; num1 < 58; num1++)
 		for (num2 = num1 + 1; num2 < 58; num2++)
 		{
 			putchar(num1);
