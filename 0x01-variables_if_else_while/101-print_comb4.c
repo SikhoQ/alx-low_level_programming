31911404b7ce5d1168e7c16100ff143e7795bd4c
SikhoQ <qangulesikho@gmail.com>
2022-09-23 15:02:12 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int	main(void)
 		for (num2 = num1 + 1; num2 < 58; num2++)
 			for (num3 = num2 + 1; num3 < 58; num3++)
 			{
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
 				if (num1 != 55)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-				putchar(num1);
-				putchar(num2);
-				putchar(num3);
 			}
 	putchar('\n');
 	return (0);
