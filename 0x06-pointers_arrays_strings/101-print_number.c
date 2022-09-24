429a8d5ddb996ee84a005f1ce92be0915ce9ffd8
SikhoQ <qangulesikho@gmail.com>
2022-09-23 17:07:29 -0700

    printing numbers 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#include <limits.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: the integer to be printed.
+ */
+void	print_number(int n)
+{
+	int	copy_of_n;
+	int	divisor;
+
+	divisor = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == INT_MIN)
+		{
+			n += 2000000000;
+			_putchar('2');
+		}
+		n = -n;
+	}
+	copy_of_n = n;
+	while (copy_of_n >= 10)
+	{
+		divisor *= 10;
+		copy_of_n /= 10;
+	}
+	while (divisor)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
+
