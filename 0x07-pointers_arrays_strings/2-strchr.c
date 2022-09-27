c3f328c11da3f6c10ab8c0650ae5a646efe57df4
SikhoQ <qangulesikho@gmail.com>
2022-09-26 17:50:07 -0700

    1st commit strchr task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: pointer to the string in which to search.
+ * @c: character to look for in s.
+ *
+ * Return: pointer to the first occurrnce of c in s, or NULL if c is not found.
+ */
+char	*_strchr(char *s, char c)
+{
+	int	i;
+
+	for (i = 0; s[i]; i++)
+		if (c == s[i])
+			return (s + i);
+	return (NULL);
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int	_putchar(char);
 char	*_memset(char*, char, unsigned int);
 char	*_memcpy(char*, char*, unsigned int);
+char	*_strchr(char*, char);
 
