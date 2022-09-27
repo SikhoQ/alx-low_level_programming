a1c55181155177462ca5722aca1ac5a0b7fb636b
SikhoQ <qangulesikho@gmail.com>
2022-09-26 20:50:34 -0700

    1st commit strstr task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr - locates a substring within a string.
+ *
+ * @haystack: pointer to the string to search in.
+ * @needle: pointer to the substring to search for.
+ *
+ * Return: pointer to the byte in haystack where needle begins
+ * or NULL if needle is not found within haystack.
+ */
+char	*_strstr(char *haystack, char *needle)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (haystack[i])
+	{
+		j = 0;
+		while (needle[j])
+			if (haystack[i + j] != neddle[j])
+				break;
+		if (!needle[j])
+			return (haystack + i);
+	}
+	return (NULL);
+}
+
