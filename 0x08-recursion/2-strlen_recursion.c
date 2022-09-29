a240f8762c2d616727639d3cd2fc22c4333aa22e
SikhoQ <qangulesikho@gmail.com>
2022-09-28 19:11:03 -0700

    3rd task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,7 +13,10 @@ int	_strlen_recursion(char *s)
 
 	length = 0;
 	if (*s)
-		length = ++length + _strlen_recursion(s + 1);
+	{
+		++length;
+		length += _strlen_recursion(s + 1);
+	}
 	return (length);
 }
 
